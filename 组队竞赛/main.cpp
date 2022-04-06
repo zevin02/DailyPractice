7bf4972f557eb511e143682e42c15e251193568a
zevin02 <1833195090@qq.com>
2022-04-06 19:48:30 +0800

    每日一题之组队竞赛

---
+++ 组队竞赛/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+#include<algorithm>
+int main()
+{
+    int n;
+    cin>>n;
+    vector<vector<int>> res;
+    res.resize(n);
+    vector<int> num;
+    int k=n*3;
+    while(k--)
+    {
+    int tmp;
+    cin>>tmp;
+    num.push_back(tmp);
+    }
+    sort(num.begin(),num.end(),greater<int>());
+     
+    long long  ans=0;
+for(int i=0;i<n;i++)
+{
+ans+=num[2*i+1];
+}
+        cout<<ans<<endl;
+     
+     
+return 0;
+}
